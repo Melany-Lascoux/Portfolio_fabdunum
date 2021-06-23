8757d9d88ed1beb2250149e3a77c60b980afb127
Lucyferra <melany.lascoux@gmail.com>
2021-06-23 13:33:34 +0200

    Modification portfolio

--- Portfolio/profil.php
+++ Portfolio/profil.php
@@ -6,7 +6,7 @@
     <div class="leftprofil">
         <h3>Profil</h3>
         <div class="img">
-            <img src="img_photo.png" alt="photo">
+            <img src="images/img_photo.png" alt="photo">
         </div>
         <h4>A propos de moi</h4>
         <p>Texte</p>
